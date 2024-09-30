154e77639f4f1e07ca89995f923b99d96382e7d5
sergey shishlov <shishlovsergey040593@gmail.com>
2024-09-30 15:49:21 +0300

    add facebook pixel

--- app.vue
+++ app.vue
@@ -9,6 +9,15 @@
       ></iframe>
     </noscript>
 
+    <noscript>
+      <img
+        height="1"
+        width="1"
+        style="display: none"
+        src="https://www.facebook.com/tr?id=1952136378533536&ev=PageView&noscript=1"
+      />
+    </noscript>
+
     <the-header />
 
     <the-cart-modal v-if="state.showModalWindow" @change-state="closeModal" />
@@ -92,6 +101,22 @@ useHead({
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
       gtag('config', 'AW-16713078315');`,
+      type: "text/javascript",
+      charset: "utf-8",
+    },
+
+    {
+      hid: "fb-pixel",
+      innerHTML: `!function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window,document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '1952136378533536');
+      fbq('track', 'PageView');`,
       type: "text/javascript",
       charset: "utf-8",
     },
